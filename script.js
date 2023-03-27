85f631dfa950641bf4bfb63e4a0a17c090926349
Kiirynn <kieran.poropat@gmail.com>
2023-03-27 15:13:35 -0400

    linked list practice && learning how to build stacks

--- script.js
+++ script.js
@@ -1,197 +1,240 @@
 
+// // LINKED LIST
 
-// declare list
+// // declare list
 
-    class linkedList{
-        constructor(){
-        this.head = null
-        this.tail = null
-        this.size = 0
-        }
+//     class linkedList{
+//         constructor(){
+//         this.head = null
+//         this.tail = null
+//         this.size = 0
+//         }
 
-// prepend
+// // prepend
 
-    insertFirst(data){
-        let newNode = new Node(data)
-        if(this.size === 0){
-            this.head = newNode
-            this.tail = newNode
-        } else{
-            newNode.next = this.head
-            this.head = newNode
-        }
-        this.size++
+//     insertFirst(data){
+//         let newNode = new Node(data)
+//         if(this.size === 0){
+//             this.head = newNode
+//             this.tail = newNode
+//         } else{
+//             newNode.next = this.head
+//             this.head = newNode
+//         }
+//         this.size++
 
-    }
+//     }
 
-// append
+// // append
 
-    insertLast(data){
-        let newNode = new Node(data)
-        if(this.size === 0 || this.head === null || this.tail === null ){    // you can use any of these
-           this.head = newNode
-           this.tail = newNode
-        } 
-        if(this.size > 0){
-            const temp = this.tail
-            this.tail = newNode
-            temp.next = this.tail;
-        }
-        this.size++
-    }
+//     insertLast(data){
+//         let newNode = new Node(data)
+//         if(this.size === 0 || this.head === null || this.tail === null ){    // you can use any of these
+//            this.head = newNode
+//            this.tail = newNode
+//         } 
+//         if(this.size > 0){
+//             const temp = this.tail
+//             this.tail = newNode
+//             temp.next = this.tail;
+//         }
+//         this.size++
+//     }
 
-// insert at a specific index
+// // insert at a specific index
 
-    insertAt(data, index){
+//     insertAt(data, index){
       
-      if(index < 0 || index > this.size){
-        return 
-      } 
-      if(this.size === 0){
-          return this.insertFirst(data)
-      } 
-      else if(index === this.size){
-        return this.insertLast(data)
-      } else{
-        let newNode = new Node(data)
-          let current = this.head
-          let prev = null
-          let counter = 0
-          while(counter < index){
-               prev = current
-               current = current.next
-               counter++
-          }
-          newNode.next = current
-          prev.next = newNode
-          this.size++
-      }
-    }
+//       if(index < 0 || index > this.size){
+//         return 
+//       } 
+//       if(this.size === 0){
+//           return this.insertFirst(data)
+//       } 
+//       else if(index === this.size){
+//         return this.insertLast(data)
+//       } else{
+//         let newNode = new Node(data)
+//           let current = this.head
+//           let prev = null
+//           let counter = 0
+//           while(counter < index){
+//                prev = current
+//                current = current.next
+//                counter++
+//           }
+//           newNode.next = current
+//           prev.next = newNode
+//           this.size++
+//       }
+//     }
 
-// remove first node
-    removeFirst(){
-        let dataVar = this.head.data   // this just saves the header data in a variable
-        if(this.size === 0){
-           return null
-        }
-        if(this.size === 1){
-            this.head = null
-            this.tail = null
-        } 
-        else{
-            this.head = this.head.next
-            this.size--
-            return dataVar
+// // remove first node
+//     removeFirst(data){
+//         let dataVar = this.head.data   // this just saves the header data in a variable
+//         if(this.size === 0){
+//            return null
+//         }
+//         if(this.size === 1){
+//             this.head = null
+//             this.tail = null
+//         } 
+//         else{
+//             this.head = this.head.next
+//             this.size--
+//             return dataVar
 
-        }
+//         }
         
       
        
-    }
+//     }
 
-    // remove last node
+//     // remove last node
 
 
-    removeLast(){
+//     removeLast(){
         
-        let dataVar = this.tail.data
-        if(this.size === 0 || this.head === null || this.tail === null ){   // you can use any of these conditionals
-            return null
-         } 
-         if(this.size === 1){
-           this.head = null
-           this.tail = null
-         } else {
-            let current = this.head
-            while(current.next.next != null){      // this makes it so you can access the second last node
-                current = current.next
+//         let dataVar = this.tail.data
+//         if(this.size === 0 || this.head === null || this.tail === null ){   // you can use any of these conditionals
+//             return null
+//          } 
+//          if(this.size === 1){
+//            this.head = null
+//            this.tail = null
+//          } else {
+//             let current = this.head
+//             while(current.next.next != null){      // this makes it so you can access the second last node
+//                 current = current.next
 
-            } 
-            current.next = null      // see notes
-            this.tail = current
-        }
-         this.size--
-         return dataVar
-    }
+//             } 
+//             current.next = null      // see notes
+//             this.tail = current
+//         }
+//          this.size--
+//          return dataVar
+//     }
 
 
-// remove at a specific index
+// // remove at a specific index
 
-    removeAt(index){
-        let dataVar = this.head.data
-        if(index < 0 || index > this.size){
-                return null
-            } 
-            if(this.size === 0){
-                return this.removeFirst()
-            } 
-            else if(index === this.size -1){
-                return this.removeLast()
-            }
-            else{
-                let current = this.head
-                let counter = 0
-                let prev = null
-              while(counter < index){
-                    prev = current
-                    current = current.next
-                    counter++
-                }
-                prev.next = current
+//     removeAt(index){
+//         let dataVar = this.head.data
+//         if(index < 0 || index > this.size){
+//                 return null
+//             } 
+//             if(this.size === 0){
+//                 return this.removeFirst()
+//             } 
+//             else if(index === this.size -1){
+//                 return this.removeLast()
+//             }
+//             else{
+//                 let current = this.head
+//                 let counter = 0
+//                 let prev = null
+//               while(counter < index){
+//                     prev = current
+//                     current = current.next
+//                     counter++
+//                 }
+//                 prev.next = current
                 
-                this.size--
-                return dataVar
-             } 
-        }
-
-// print list
-   
-    // printList(){
-    //     let current = this.head
-
-    //    while(current){
-    //     current = current.next
-    //     console.log(current.data)
-    //    }
-    // }
+//                 this.size--
+//                 return dataVar
+//              } 
+//         }
 
 
-    // print list 2
 
-     printList2(){
-        let data = ""
-        let current = this.head
-        while(current != null){ 
-            data = data + current.data + " "
-            current = current.next
-    }
-         console.log(data)
-    }
+//     // print list 
+
+//      printList(){
+//         let data = ""
+//         let current = this.head
+//         while(current != null){ 
+//             data = data + current.data + " "
+//             current = current.next
+//     }
+//          console.log(data)
+//     }
  
 
 
-}
+// }
 
-// construct node
+// // construct node
 
-class Node{
-    constructor(data){
-       this.data = data
-       this.next = null
+// class Node{
+//     constructor(data){
+//        this.data = data
+//        this.next = null
      
-    }
-}
+//     }
+// }
 
-const ll = new linkedList();
+// const ll = new linkedList();
 
-ll.insertFirst(10)
-ll.insertLast(20)
-ll.insertLast(30)
-ll.insertAt(50, 2)
-ll.insertLast(68)
+// ll.insertFirst(10)
+// ll.insertLast(20)
+// ll.insertLast(30)
+// ll.insertAt(50, 2)
+// ll.insertLast(68)
 
-ll.removeLast()
-
-ll.printList2()
+// ll.removeLast()
 
 // ll.printList()
+
+
+
+// STACK
+
+ class Stack {
+    constructor(){
+        this.items = []
+        
+    }
+
+   push(item){
+      this.items.push(item)
+    //   let data = this.storage[this.top] 
+   }
+
+
+   pop(){
+       return this.items.pop()
+   }
+
+   peek(){
+    if(this.items.length === 0){
+        return null
+    } else{
+        return this.items[this.items.length - 1]
+       }
+      
+   }
+
+   getSize(){
+    return this.items.length
+ }
+
+   isEmpty(){
+      return  this.getSize() === 0
+   }
+
+
+}
+
+let stack = new Stack();
+
+
+stack.push("honda")
+stack.push("toyata")
+stack.push("ferrari")
+stack.push("chevy")
+
+
+console.log(stack)
+
+
+// QUEUE
